5753893fa95d208e2b3c60d68ad679f9971d88a4
Andrew Defee <andrew@andrewdefee.com>
2026-04-09 05:35:11 -0500

    ui test fixes (2)

--- packages/ui/src/services/identityUnlockFlow.test.ts
+++ packages/ui/src/services/identityUnlockFlow.test.ts
@@ -1,29 +1,9 @@
-import { afterAll, describe, expect, mock, test } from 'bun:test';
-import * as realCrypto from '@adieuu/crypto';
-import * as realDeviceKeyStorage from './deviceKeyStorage';
-
-const deriveEntropyWrappingKeyMock = mock(async () => new Uint8Array([1, 2, 3]));
-const getOrCreateWrappingSaltMock = mock(async () => new Uint8Array([9, 9, 9]));
-
-mock.module('@adieuu/crypto', () => ({
-  ...realCrypto,
-  deriveEntropyWrappingKey: deriveEntropyWrappingKeyMock,
-}));
-
-mock.module('./deviceKeyStorage', () => ({
-  ...realDeviceKeyStorage,
-  getOrCreateWrappingSalt: getOrCreateWrappingSaltMock,
-}));
-
-const unlockFlow = await import('./identityUnlockFlow');
-
-afterAll(() => {
-  mock.restore();
-});
+import { describe, expect, test } from 'bun:test';
+import { deriveUnlockWrappingKey } from './identityUnlockFlow';
 
 describe('identityUnlockFlow', () => {
   test('derives wrapping key and salt', async () => {
-    const result = await unlockFlow.deriveUnlockWrappingKey('id-1', 'pw');
+    const result = await deriveUnlockWrappingKey('id-1', 'pw');
     expect(result.ok).toBe(true);
   });
 });
