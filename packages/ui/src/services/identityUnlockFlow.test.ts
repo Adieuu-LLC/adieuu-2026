19a596d15acd3ba5f4343f4b8d8a0891eff983af
Andrew Defee <andrew@andrewdefee.com>
2026-04-09 05:21:07 -0500

    ui test fixes

--- packages/ui/src/services/identityUnlockFlow.test.ts
+++ packages/ui/src/services/identityUnlockFlow.test.ts
@@ -1,13 +1,17 @@
 import { afterAll, describe, expect, mock, test } from 'bun:test';
+import * as realCrypto from '@adieuu/crypto';
+import * as realDeviceKeyStorage from './deviceKeyStorage';
 
 const deriveEntropyWrappingKeyMock = mock(async () => new Uint8Array([1, 2, 3]));
 const getOrCreateWrappingSaltMock = mock(async () => new Uint8Array([9, 9, 9]));
 
 mock.module('@adieuu/crypto', () => ({
+  ...realCrypto,
   deriveEntropyWrappingKey: deriveEntropyWrappingKeyMock,
 }));
 
 mock.module('./deviceKeyStorage', () => ({
+  ...realDeviceKeyStorage,
   getOrCreateWrappingSalt: getOrCreateWrappingSaltMock,
 }));
 
