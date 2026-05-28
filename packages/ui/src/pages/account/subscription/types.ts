76b9387132f97c28d8e6f0a9372c7bf31ce99dda
Andrew Defee <andrew@andrewdefee.com>
2026-05-28 18:41:57 -0500

    subscription copy comparison fixes

--- packages/ui/src/pages/account/subscription/types.ts
+++ packages/ui/src/pages/account/subscription/types.ts
@@ -78,7 +78,7 @@ export const ACCESS_FEATURES = [
 ] as const;
 
 /** Insider annual adds these on top of Access. */
-export const INSIDER_ONLY_FEATURES = ['callBiWeekly'] as const;
+export const INSIDER_ONLY_FEATURES = ['callMonthly'] as const;
 
 export const INSIDER_FEATURES = [...ACCESS_FEATURES, ...INSIDER_ONLY_FEATURES] as const;
 
